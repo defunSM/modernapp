75dbf37d56d077fa5ae3ccfeb87dc6284407ba35
defunSM <sxh8083@g.rit.edu>
2016-10-30 20:49:58 -0400

    new

--- src/clj/newchat/server.clj
+++ src/clj/newchat/server.clj
@@ -9,6 +9,9 @@
 
 (def database-connection (env :database-url "postgres://jlhkvxviinvkpy:7hkCi_Cc_4id02UMci4-TiOVeR@ec2-54-225-64-254.compute-1.amazonaws.com:5432/d7lei527gbu5v"))
 
+(defn record [db input]
+ (db/insert! database-connection db {:content input}))
+
 (defn -main [& args]
   (let [port (Integer/parseInt (or (env :port) "3000"))]
     (run-jetty app {:port port :join? false})))
