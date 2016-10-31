fedae660b290062e4dd440d5b6234130d8d509aa
defunSM <sxh8083@g.rit.edu>
2016-10-30 20:46:58 -0400

    new

--- src/clj/newchat/server.clj
+++ src/clj/newchat/server.clj
@@ -7,6 +7,8 @@
 )
   (:gen-class))
 
- (defn -main [& args]
-   (let [port (Integer/parseInt (or (env :port) "3000"))]
-     (run-jetty app {:port port :join? false})))
+(def database-connection (env :database-url "postgres://jlhkvxviinvkpy:7hkCi_Cc_4id02UMci4-TiOVeR@ec2-54-225-64-254.compute-1.amazonaws.com:5432/d7lei527gbu5v"))
+
+(defn -main [& args]
+  (let [port (Integer/parseInt (or (env :port) "3000"))]
+    (run-jetty app {:port port :join? false})))
